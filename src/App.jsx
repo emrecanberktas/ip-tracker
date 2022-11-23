59edb626f2ec86a8765dfd75ab4aa4d548a7e3af
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-11-23 16:14:18 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
