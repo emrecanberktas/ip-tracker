28cb427a093a863cce0d781908731c4b0bcc3cf0
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-11-28 14:43:17 +0300

    Chore: map component

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "./index.css";
+import dataStore from "../../store";
 
 function Map() {
-  return <div className="map">Map</div>;
+  const data = dataStore((state) => state.data);
+  if (!data) return <div>Loading...</div>;
+  const location = [data.location.lat, data.location.lng];
+  return (
+    <div className="map">
+      <MapContainer center={location} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={location}></Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
