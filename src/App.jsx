5e6fc021ee564a5a32d934701cc40641373a366f
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-11-30 23:28:54 +0300

    Chore

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
 import Map from "./components/map";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/header";
+import BlankPage from "./components/blankPage";
+import dataStore from "./store";
 
 function App() {
+  const data = dataStore((state) => state.data);
+  const setData = dataStore((state) => state.setData);
+  useEffect(() => {
+    axios.get("https://api.ipify.org?format=json").then((res) => {
+      axios
+        .get(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${
+            import.meta.env.VITE_GEOLOCATION_API_KEY
+          }&ipAddress=${res.data.ip}`
+        )
+        .then((res) => {
+          setData(res.data);
+        });
+    });
+  }, []);
+  if (!data) return <BlankPage />;
   return (
     <div className="app">
       <Header />
