94ae68d4be96f8643d91b7d41a10feed55558695
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-11-30 23:30:43 +0300

    Add: moving to new ip location

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,15 +1,27 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "./index.css";
 import dataStore from "../../store";
 
 function Map() {
+  const [map, setMap] = useState(null);
   const data = dataStore((state) => state.data);
-  if (!data) return <div>Loading...</div>;
-  const location = [data.location.lat, data.location.lng];
+  const location = [data?.location?.lat, data?.location?.lng];
+
+  useEffect(() => {
+    if (map) {
+      map.flyTo(location, 15);
+    }
+  }, [data]);
+
   return (
     <div className="map">
-      <MapContainer center={location} zoom={15} scrollWheelZoom={true}>
+      <MapContainer
+        center={location}
+        zoom={15}
+        scrollWheelZoom={true}
+        ref={setMap}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
