598b3e94ab79b9c0955dcf3cff68d11924e2454f
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-11-30 23:30:05 +0300

    Add: Loading screen

---
+++ src/components/blankPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function BlankPage() {
+  return (
+    <div className="blank-page ">
+      <div className="dots-bars-5"></div>
+    </div>
+  );
+}
+
+export default BlankPage;
